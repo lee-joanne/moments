81fa050f323b3b3ce488de0b818193c01ffe7ec7
Joanne <lee.joanne54@gmail.com>
2022-10-03 18:45:33 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
 
 export default App;
+
